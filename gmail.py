af9edf88d79bfdd8a932d4fa29c33eb3fcbede6b
Sai Sree Harsha <55547481+Harsha9075@users.noreply.github.com>
2021-06-05 20:14:39 +0530

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,12 +1,23 @@
+#Importing smtlib library for transactions between E-mails.
 import smtplib
+
+#Sender Email
 senderemail = "harshaasapu907@gmail.com"
+#Reciever Email
 recieveremail = "harshaontrade@gmail.com"
-password = input(str("Please enter your password:"))
-message = "Congratulations, you have won the lottery!."
-server = smtplib.SMTP('smtp.gmail.com',587)
+
+password = input(str("Please enter your password:"))    #Enter your password here.
+
+message = "Congratulations, you have won the lottery!."   #This message displays in the recievers inbox.
+
+server = smtplib.SMTP('smtp.gmail.com',587) #Connection with the server
 server.starttls()
+
+#Enter your credentials
 server.login(senderemail,password)
 print("------------Successfull login-------------")
+
+#Finally,Sending the mail 
 server.sendmail(senderemail,recieveremail,message)
 print("-----------Message has been delivered to",recieveremail)
 
